8ffd8de81691f2f8b473bfcf0f3b8aea378b3b1f
Carlos Fontes <mail@ccfontes.com>
2023-04-21 22:38:07 +0900

    Forgot to move dep to bb.edn

--- examples/bb-lib/handler.clj
+++ examples/bb-lib/handler.clj
@@ -1,11 +1,6 @@
 (ns function.handler
   (:require [clojure.edn :as edn]
-            [babashka.deps :as deps]
-            [babashka.tasks]))
-
-(deps/add-deps '{:deps {com.rpl/specter {:mvn/version "1.1.4"}}})
-
-(require '[com.rpl.specter :refer [select]])
+            [com.rpl.specter :refer [select]]))
 
 (defn handler [s]
   (let [v (select ["a" "b"] (edn/read-string s))]
