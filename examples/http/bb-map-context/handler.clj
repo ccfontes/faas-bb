e1cd4a2eb48f521568a3f45d76d5f23384a5497e
Carlos Fontes <mail@ccfontes.com>
2023-08-20 08:35:18 +0000

    🔧 chore(handler.clj): refactor handler function to include context parameter 🐛 fix(handler.clj): print context variable for debugging purposes

--- examples/http/bb-map-context/handler.clj
+++ examples/http/bb-map-context/handler.clj
@@ -1,4 +1,5 @@
 (ns function.handler)
 
-(defn handler [content {:keys [headers env]}]
+(defn handler [content {:keys [headers env] :as context}]
+  (println "context" context)
   [(keys content) (vals content) (:content-type headers) (:keywords env)])
