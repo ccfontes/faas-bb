11355271f932b80b821ce427d5766b73653eea65
Carlos Fontes <mail@ccfontes.com>
2023-11-06 06:29:08 +0000

    Add routing example using bell

---
+++ examples/http/bb-routes-bell/handler.clj
@@ -0,0 +1,11 @@
+(ns function.handler
+  (:require [bell.core :refer [router POST PUT]]))
+
+(def handler
+  (router
+    (POST "/"
+          (fn [{:keys [body]}]
+            [(keys body) (vals body)]))
+    (PUT "/foo/:id"
+         (fn [{:keys [path-params]}]
+           [(keys path-params) (vals path-params)]))))
