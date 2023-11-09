cc200894e1e5e56276d03874f44060f2b3fdd542
Carlos Fontes <mail@ccfontes.com>
2023-11-09 13:13:58 +0900

    Add bb hiccup example Function

---
+++ examples/http/bb-hiccup/view.clj
@@ -0,0 +1,17 @@
+(ns function.view
+  (:require [hiccup2.core :refer [html]]))
+
+(defn ->item-list [lst]
+  [:ul (for [x lst] [:li x])])
+
+(defn hiccup-page [title {:keys [item-list]}]
+  [:html
+    [:head
+      [:title title]]
+    [:body
+      [:h1 title]
+      [:div {:id "item-list"}
+        (->item-list item-list)]]])
+
+(defn render-page [title data]
+  (-> (hiccup-page title data) html str))
