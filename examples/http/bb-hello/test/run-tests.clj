ca066e53684f6832faa44ff172ca8d62fd071531
Carlos Fontes <mail@ccfontes.com>
2023-11-09 17:24:33 +0000

    Support testing Functions

---
+++ examples/http/bb-hello/test/run-tests.clj
@@ -0,0 +1,11 @@
+(ns function.test.run-tests
+  (:require [eg :refer [eg]]
+            [clojure.test :refer [run-tests]]
+            [function.handler :refer [handler]]))
+
+(eg handler
+  {:body "foo"} => {:body "Hello, foo"})
+
+(let [{:keys [fail error]} (run-tests 'function.test.run-tests)]
+  (when (pos? (+ fail error))
+    (System/exit 1)))
