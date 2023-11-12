55f3d2e9da26d15f51e2c9ef6eac378239d856f2
Carlos Fontes <mail@ccfontes.com>
2023-11-12 15:45:03 +0000

    Fix handler name

--- examples/http/bb-routes/handler.clj
+++ examples/http/bb-routes/handler.clj
@@ -1,6 +1,6 @@
 (ns function.handler)
 
-(defn app [{:keys [request-method uri]}]
+(defn handler [{:keys [request-method uri]}]
   (case [request-method uri]
     [:get "/"] (constantly "root")
     [:get "/foo"] (constantly "foo")))
