22121ded69e1241479c10ded300141020c72f6e7
Carlos Fontes <mail@ccfontes.com>
2023-11-21 05:04:38 +0000

    rm redundant code

--- examples/http/bb-routes-bell/test/run_tests.clj
+++ examples/http/bb-routes-bell/test/run_tests.clj
@@ -1,11 +1,8 @@
 (ns function.test.run-tests
   (:require
-    [clojure.test :refer [run-tests]]
-    [eg :refer [eg]]
-    [function.handler :refer [handler]]))
+    [clojure.test :refer [run-tests]]))
 
 (defn -main []
   (let [{:keys [fail error]} (run-tests 'function.test.run-tests)]
     (when (pos? (+ fail error))
       (System/exit 1))))
-
