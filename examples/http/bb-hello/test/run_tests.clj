2a801944894a4a36707270d251e8ec8aded93933
Carlos Fontes <mail@ccfontes.com>
2023-11-21 04:00:40 +0000

    Wrap final bits

--- examples/http/bb-hello/test/run_tests.clj
+++ examples/http/bb-hello/test/run_tests.clj
@@ -8,7 +8,6 @@
   {:body "anything"} "Hello OpenFaaS!")
 
 (defn -main []
-  (System/exit (run-tests 'function.test.run-tests))
   (let [{:keys [fail error]} (run-tests 'function.test.run-tests)]
     (when (pos? (+ fail error))
       (System/exit 1))))
--- template/bb/function/test/run_tests.clj
+++ template/bb/function/test/run_tests.clj
@@ -1,6 +1,7 @@
 (ns function.test.run-tests
   (:require [clojure.test :refer [run-tests]]))
 
-(let [{:keys [fail error]} (run-tests 'function.test.run-tests)]
-  (when (pos? (+ fail error))
-    (System/exit 1)))
+(defn -main []
+  (let [{:keys [fail error]} (run-tests 'function.test.run-tests)]
+    (when (pos? (+ fail error))
+      (System/exit 1))))
